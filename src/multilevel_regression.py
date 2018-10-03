0819261e7a4aa629d764ad892083318e2b329339
Andrei Kucharavy <andrei.chiffa136@gmail.com>
2018-10-03 14:39:08 -0400

    Added the final slide and adjusted the columns for PyMMT/C1tag organoids

--- src/multilevel_regression.py
+++ src/multilevel_regression.py
@@ -32,26 +32,27 @@ def analysis_loop(data_table):
         return r_value**2, p_value
 
     headers = ['ID',
-               'Invasion',
-               'Size',
-               'K14 Peripheral Pixels Sum',
-               'K14 Central Pixels Sum',
-               'K14 Entire Sum',
-               'K14 Entire Mean']
+                'Invasion',
+                'Size',
+                'K14 Peripheral Pixels Sum',
+                'K14 Central Pixels Sum',
+                'K14 Entire Sum',
+                "K14 Total Mean	",
+                "K14 Peripheral Mean",
+                "K14 Central Mean"]
 
-    r2_matrix = np.zeros((5, 5))
+    r2_matrix = np.zeros((7, 7))
 
-    exp_var_matrix = np.zeros((5, 5))
-
+    exp_var_matrix = np.zeros((7, 7))
 
     base_var = np.var(data_table[:, 1])
 
-    for i in range(2, 7):
+    for i in range(2, 9):
         slope_1, intercept_1, r_value_1, p_value_1, std_err_1 = stats.linregress(data_table[:, i],
                                                                        data_table[:, 1])
         remaining_variance = data_table[:, 1] - intercept_1 - data_table[:, i]*slope_1
         explained_variance = (1 - np.var(remaining_variance) / base_var) * 100
-        print '\t%s: r2: %.2f, p_val: %.2f, rem var: %.2f %%' % (headers[i],
+        print '\t%s: r2: %.2f, p_val: %.2e, rem var: %.2f %%' % (headers[i],
                                                               r_value_1**2,
                                                               p_value_1,
                                                               explained_variance)
@@ -59,17 +60,13 @@ def analysis_loop(data_table):
         r2_matrix[i-2, i-2] = r_value_1**2
         exp_var_matrix[i-2, i-2] = explained_variance
 
-        for j in range(2, 7):
+        for j in range(2, 9):
             if i != j:
                 slope_2, intercept_2, r_value_2, p_value_2, std_err_2 = stats.linregress(
                     data_table[:, j],
                     remaining_variance)
                 remaining_variance_2 = remaining_variance - intercept_2 - data_table[:, j]*slope_2
                 explained_variance_2 = (1 - np.var(remaining_variance_2) / base_var) * 100
-                print '\t\t%s: r2: %.2f, p_val: %.2f, rem var: %.2f %%' % (headers[j],
-                                                                        r_value_2**2,
-                                                                        p_value_2,
-                                                                        explained_variance_2)
 
                 r2_matrix[j-2, i-2] = r_value_2**2
                 r2_matrix[i-2, j-2] = r_value_2**2
@@ -114,9 +111,10 @@ def analysis_loop(data_table):
                 # plt.show()
 
     np.set_printoptions(precision=2, suppress=True)
-    print r2_matrix
+    # print r2_matrix
+    #
+    # print exp_var_matrix
 
-    print exp_var_matrix
 
 if __name__ == "__main__":
     data_tables = [parse_data_summary(file_location) for file_location in mouse_data_files]
